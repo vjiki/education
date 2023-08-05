df2217ecc401bffa64ec56dd27a6e91078a5bc12
Nikolai Golubkin <nikolai.golubkin@orioninc.com>
2023-08-05 18:07:49 +0200

    epam: dao

--- yandex/algorithms/inputparams/build.gradle.kts
+++ yandex/algorithms/inputparams/build.gradle.kts
@@ -12,6 +12,9 @@ repositories {
 dependencies {
     implementation("org.junit.jupiter:junit-jupiter-api:5.10.0")
     implementation("org.junit.jupiter:junit-jupiter-api:5.10.0")
+    implementation("junit:junit:4.13.2")
+    implementation("junit:junit:4.13.2")
+    implementation("junit:junit:4.13.2")
     testImplementation(platform("org.junit:junit-bom:5.9.1"))
     testImplementation("org.junit.jupiter:junit-jupiter")
 }
